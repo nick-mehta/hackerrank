411bba621155085c8e176a481808fb0ed2975c74
nick-mehta <59080769+nick-mehta@users.noreply.github.com>
2019-12-29 11:01:14 +0530

    Add files via upload

---
+++ conditional if else.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+    if (n > 9){
+        cout << "Greater than 9" <<endl;
+    }
+
+    else if (n==1){
+        cout << "one"<< endl;
+    }
+
+    else if (n==2){
+        cout << "two"<< endl;
+    }
+
+    else if (n==3){
+        cout << "three"<< endl;
+    }
+
+    else if (n==4){
+        cout << "four"<< endl;
+    }
+
+    else if (n==5){
+        cout << "five"<< endl;
+    }
+
+    else if (n==6){
+        cout << "six"<< endl;
+    }
+
+    else if (n==7){
+        cout << "seven"<< endl;
+    }
+
+    else if (n==8){
+        cout << "eight"<< endl;
+    }
+
+    else if (n==9){
+        cout << "nine"<< endl;
+    }
+
+
+    return 0;
+}
